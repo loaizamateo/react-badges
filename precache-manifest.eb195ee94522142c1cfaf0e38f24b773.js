29095bde4c59002c44c7721091b35ed04108e090
Mateo Loaiza Rios <mateoloaiza1227@gmail.com>
2020-11-09 15:46:37 -0500

    Updates

---
+++ precache-manifest.eb195ee94522142c1cfaf0e38f24b773.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1a901b3731c8e4574c8d",
+    "url": "/react-badges/static/css/main.70a229d3.chunk.css"
+  },
+  {
+    "revision": "1ca7e770b323ef47f898",
+    "url": "/react-badges/static/js/runtime~main.1ca7e770.js"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "b4e7c4889234b4716003",
+    "url": "/react-badges/static/js/2.b4e7c488.chunk.js"
+  },
+  {
+    "revision": "1a901b3731c8e4574c8d",
+    "url": "/react-badges/static/js/main.1a901b37.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "b4e7c4889234b4716003",
+    "url": "/react-badges/static/css/2.30fb27be.chunk.css"
+  },
+  {
+    "revision": "7b7963349b9dae24388cb9e53db15a6e",
+    "url": "/react-badges/index.html"
+  }
+];
